03124b4e81f58d0a7e18fff1b59b73541c32e710
David <davidlago104@gmail.com>
2021-03-03 16:30:24 -0500

    added redux, thunk, router-dom

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-class App extends React.Component() {
-  render() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
-}
+class App extends React.Component {
 
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/sneakers', {
+  //     method: 'GET'
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+      
+  // }
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+  }
 }
 
 export default App;
