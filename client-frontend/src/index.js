459f13086032657d09ccac27ed736a763c9dd812
David <davidlago104@gmail.com>
2021-03-03 16:35:25 -0500

    imported redux to index.js for store

--- client-frontend/src/index.js
+++ client-frontend/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'redux-thunk';
+
 import App from './App';
 
 ReactDOM.render(
@@ -9,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// need to create a store with redux
