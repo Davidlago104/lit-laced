34e091ef43362cfe83612862c65e5a1a851d3fda
David <davidlago104@gmail.com>
2021-03-05 19:23:25 -0500

    added SneakerInput.js to components

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 import fetchSneakers from './actions/fetchSneakers'
 import SneakerContainer from './containers/SneakerContainer'
+import SneakerInput from './components/SneakerInput'
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
         <SneakerContainer/>
+        <SneakerInput/>
       </div>
     );
   }
---
+++ client-frontend/src/components/SneakerInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SneakerInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                SneakerInput
+            </div>
+        )
+    }
+}
+
+export default SneakerInput;
