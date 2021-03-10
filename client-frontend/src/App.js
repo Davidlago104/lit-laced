2704734123a93ef2839a46491fbd91846e5d4d2d
David <davidlago104@gmail.com>
2021-03-10 18:27:28 -0500

    created a form within SneakerInput, created change function, about to create submit function to submit data to the backend

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <SneakerContainer/>
-        {/* <SneakerInput/> */}
+        <SneakerInput/>
       </div>
     );
   }
--- client-frontend/src/components/SneakerInput.js
+++ client-frontend/src/components/SneakerInput.js
@@ -2,10 +2,31 @@ import React from 'react';
 
 class SneakerInput extends React.Component {
 
+    state = {name: "", price: ""}
+
+    change = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value 
+        })
+    }
+
+    submit = () => {
+        
+    }
+
     render() {
         return (
             <div>
-                SneakerInput
+                <form onSubmit={this.submit}>
+                    <label>Sneaker Name</label><br/>
+                    <input type="text" placeholder="name" name="name" value={this.state.name} onChange={this.change}/>
+                    <br/>
+                    <br/>
+                    <label>Sneaker Price</label><br/>
+                    <input type="text" placeholder="price" name="price" value={this.state.price} onChange={this.change}/>
+                    <br/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
